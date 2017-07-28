6e73db57775e6c1cd49497fc092bc8117d060435
isasi88 <kikeisasi@gmail.com>
2017-07-28 02:58:49 +0200

    Add css fixes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,8 +13,14 @@ class PagesController < ApplicationController
 	def finances
 		@user = current_user
 		@projects = @user.projects.all
-		@estimated = @user.projects.pluck(:price).sum
-		@estimated_iva = @user.projects.pluck(:iva).sum
-		@totalprice = @estimated + @estimated_iva
+		if @projects
+			@estimated = @user.projects.pluck(:price).sum
+			@estimated_iva = @user.projects.pluck(:iva).sum
+			@totalprice = @estimated + @estimated_iva
+		else
+			@estimated = 0
+			@estimated_iva = 0
+			@totalprice = 0
+		end
 	end
 end
