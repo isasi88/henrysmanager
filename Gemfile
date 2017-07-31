37b37d8c25211d105d59f99c7b14670a14ef5329
isasi88 <kikeisasi@gmail.com>
2017-07-31 23:53:21 +0200

    Add data tables

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 gem "chartkick"
 gem 'devise'
 gem 'best_in_place'
+gem 'jquery-datatables-rails', '~> 3.3.0'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.7'
 # Use postgresql as the database for Active Record
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,12 +12,48 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require dataTables/jquery.dataTables
+//= require dataTables/extras/dataTables.responsive
+//= require dataTables/jquery.dataTables
+//= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
+//= require dataTables/jquery.dataTables
 //= require Chart.bundle
 //= require chartkick
 //= require best_in_place
 //= require jquery.purr
 //= require turbolinks
+
 //= require_tree .
 
 
-  $('.best_in_place').best_in_place()
+  $('.best_in_place').best_in_place();
+
+
+$(document).ready(function(){
+table = $('#clientes').DataTable();
+table.destroy();
+$('#clientes').DataTable({
+    language: {
+        processing:     "Procesando...",
+        search:         "Buscar&nbsp;:",
+        lengthMenu:    "Mostrar _MENU_ elementos",
+        info:           "Mostrando _START_ a _END_ de _TOTAL_ elementos",
+        infoEmpty:      "Affichage de l'&eacute;lement 0 &agrave; 0 sur 0 &eacute;l&eacute;ments",
+        infoFiltered:   "(filtrados de _MAX_ elementos del total)",
+        infoPostFix:    "",
+        loadingRecords: "Cargando...",
+        zeroRecords:    "No hay datos",
+        emptyTable:     "No hay datos",
+        paginate: {
+            first:      "Primero",
+            previous:   "Anterior",
+            next:       "Siguiente",
+            last:       "Ultimo"
+        },
+        aria: {
+            sortAscending:  ": activer pour trier la colonne par ordre croissant",
+            sortDescending: ": activer pour trier la colonne par ordre décroissant"
+    }
+	}
+	});
+  });
